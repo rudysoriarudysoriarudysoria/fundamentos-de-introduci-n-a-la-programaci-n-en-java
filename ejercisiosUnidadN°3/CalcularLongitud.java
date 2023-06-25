8fe1dfad1cb916969466b2e128dea7c584c1d278
rudysoriarudysoriarudysoria <137706122+rudysoriarudysoriarudysoria@users.noreply.github.com>
2023-06-25 11:55:30 -0400

    Add files via upload
    ejercisios de la unidad 3, "me fallo al adjuntar con git"  :(

---
+++ ejercisiosUnidadN°3/CalcularLongitud.java
@@ -0,0 +1,7 @@
+public class CalcularLongitud{
+    public int calcular(){
+        int longitud=20;
+        int resultado=longitud*2;
+        return resultado;
+    }
+}
---
+++ ejercisiosUnidadN°3/Insomnio.java
@@ -0,0 +1,24 @@
+public class Insomnio{
+    
+    public int calcularTiempo(){
+        int horaQueDurmio=2;
+        int minutoQueDurmio=45;
+        int horaQueDesperto=11;
+        int minutoQueDespeto=25;
+        int horaQueDurmioEnMinutos=horaQueDurmio*60+minutoQueDurmio;
+        int horaQueDesperoEnMinutos=horaQueDesperto*60+minutoQueDespeto;
+        int resultado=horaQueDesperoEnMinutos-horaQueDurmioEnMinutos;
+        return resultado;
+     
+        
+        
+        
+    
+    
+    
+    
+    
+    
+      
+    }
+}
